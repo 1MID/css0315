7255daa4f4b32eb746c7670970b3a9c11a398cf6
Ray <LT1stSoloMID@gmail.com>
2021-03-15 10:51:53 +0800

    css 0315 draft

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
+import './Reset.css'
 import './App.css';
+import './Designtest.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="CSSform2">
+
+      <div class="menu">
+          <div class="title">
+              <img class="myphoto"/>
+              <span>葉昕濠</span>
+          </div>
+          <ul>
+              <li>Home</li>
+              <li>Pag1</li>
+              <li>Pag2</li>
+              <li>Pag3</li>
+          </ul>
+          <div class="link">
+              <div class="fb"></div>
+              <div class="github"></div>
+              <div class="twitter"></div>
+          </div>
+      </div>
+      
+      <div class="content">
+          <div class="banner">
+            <div class="div">
+              <div class="myphoto"></div> 
+              <div><h1>葉昕濠</h1><br/><span>網頁前端切版練習</span></div>
+            </div>
+          </div> 
+
+          <div class="news">
+            <div class="post-title">最新文章</div>
+            <ul>
+                <li>CSS切版練習的心得分享！</li> 
+                <li>前端框架的運用及實際操作教學！</li>  
+                <li>如何後端串接資料庫與資料庫正規化？</li>  
+            </ul>
+
+            <a href="https://project0313.herokuapp.com/" class="link-button">More</a>
+
+          </div>
+
+
+      </div>
+      
     </div>
   );
 }
